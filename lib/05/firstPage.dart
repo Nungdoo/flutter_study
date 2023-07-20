61481f88e897c8dcb3d2b48b957d0b46f6244e73
nungdoo <hola.coco93@gmail.com>
2023-07-20 23:17:33 +0900

    add touch gesture

--- lib/05/firstPage.dart
+++ lib/05/firstPage.dart
@@ -16,18 +16,29 @@ class FirstApp extends StatelessWidget {
           // BuildContext는 위젯 트리에서 위젯의 위치를 알려줌 -> context
           // int는 아이템의 순번을 의미함 -> position
           child: ListView.builder(itemBuilder: (context, position) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Image.asset(
-                    list![position].imagePath!,
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.contain
-                  ),
-                  Text(list![position].animalName!)
-                ],
-              )
+            return GestureDetector(
+              child: Card(
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset(
+                          list![position].imagePath!,
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.contain
+                      ),
+                      Text(list![position].animalName!)
+                    ],
+                  )
+              ),
+              onTap: () {
+                AlertDialog dialog = AlertDialog(
+                  content: Text('이 동물은 ${list![position].kind}입니다', style: TextStyle(fontSize: 30.0),),
+                );
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) => dialog
+                );
+              },
             );
           },
           // 아이템 개수만큼만 스크롤 할 수 있게 제한
