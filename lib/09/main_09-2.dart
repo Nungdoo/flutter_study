3319c994595efa5d7d87d587e68929b828431886
nungdoo <hola.coco93@gmail.com>
2023-07-26 15:21:30 +0900

    save data with fileIO

---
+++ lib/09/main_09-2.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FileApp()
+    );
+  }
+}
+
+class FileApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FileApp();
+}
+
+class _FileApp extends State<FileApp> {
+  int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    readCountFile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('File Example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            '$_count',
+            style: TextStyle(fontSize: 40)
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+          writeCountFile(_count);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void writeCountFile(int count) async {
+    var dir = await getApplicationDocumentsDirectory();
+    File(dir.path + '/count.txt').writeAsStringSync(count.toString());
+  }
+
+  void readCountFile() async {
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      var file = await File(dir.path + '/count.txt').readAsString();
+      print(file);
+
+      setState(() {
+        _count = int.parse(file);
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
