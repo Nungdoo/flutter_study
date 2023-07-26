ebd49331594047bf263400ec0046015fd2c289a5
nungdoo <hola.coco93@gmail.com>
2023-07-26 21:42:53 +0900

    load data with shared_preferences and fileIO

--- lib/09/main_09-2.dart
+++ lib/09/main_09-2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,11 +29,14 @@ class FileApp extends StatefulWidget {
 
 class _FileApp extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = new List.empty(growable: true);
+  TextEditingController controller = new TextEditingController();
 
   @override
   void initState() {
     super.initState();
     readCountFile();
+    initData();
   }
 
   @override
@@ -43,18 +47,35 @@ class _FileApp extends State<FileApp> {
       ),
       body: Container(
         child: Center(
-          child: Text(
-            '$_count',
-            style: TextStyle(fontSize: 40)
-          ),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                // ListView.builder는 내용에 따라 알아서 크기를 조절해서 배치하지 못해, Expanded 위젯 사용
+                // 남은 공간 전부 사용함
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Center(
+                        child: Text(
+                          itemList[index],
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: itemList.length,
+                ),
+              ),
+            ],
+          )
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            _count++;
-          });
-          writeCountFile(_count);
         },
         child: Icon(Icons.add),
       ),
@@ -77,6 +98,48 @@ class _FileApp extends State<FileApp> {
       });
     } catch (e) {
       print(e.toString());
+    }
+  }
+
+  void initData() async {
+    // initState() 함수에선 async를 사용할 수 없어, 별도의 initData() 함수를 만듦
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
+  }
+
+  // fruit.txt 파일에서 데이터를 읽어와
+  // 내부 저장소인 공유 환경설정에 저장한 다음 리스트를 만듦
+  Future<List<String>> readListFile() async {
+    List<String> itemList = new List.empty(growable: true);
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key); // 파일을 처음 열었는지 확인하는 용도
+    var dir = await getApplicationDocumentsDirectory();
+    bool fileExist = await File(dir.path + '/fruit.txt').exists(); // 파일이 있는지 확인하는 용도
+
+    // readListFile() 함수를 처음 실행하면
+    // 애셋에 등록한 파일을 읽어서 내부 저장소에 다시 저장함
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true); // 파일을 연 것으로 기록
+      var file = await DefaultAssetBundle.of(context).loadString('repo/fruit.txt'); // 애셋에 등록한 repo/fruit.txt 파일을 읽음
+
+      File(dir.path + '/fruit.txt').writeAsStringSync(file); // repo/fruit.txt 와 똑같은 파일을 만듦
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    } else { // 내부 저장소에 있는 파일(/fruit.txt) 데이터를 불러와 리스트를 만듦
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
     }
   }
 }
