46c13ab803e6ae62a02e2886d14db77c3f8b619d
nungdoo <hola.coco93@gmail.com>
2023-07-30 13:44:39 +0900

    add opacity change animation

--- lib/11/main_11.dart
+++ lib/11/main_11.dart
@@ -25,6 +25,7 @@ class _AnimationApp extends State<AnimationApp> {
   List<People> peoples = new List.empty(growable: true);
   Color weightColor = Colors.blue;
   int current = 0;
+  double _opacity = 1;
 
   @override
   void initState() {
@@ -47,48 +48,52 @@ class _AnimationApp extends State<AnimationApp> {
         child: Center(
           child: Column(
             children: <Widget>[
-              SizedBox(
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(width: 100, child: Text('이름 : ${peoples[current].name}'),),
-                    AnimatedContainer(
-                      duration: Duration(seconds: 2),
-                      curve: Curves.bounceIn,
-                      color: Colors.amber,
-                      child: Text(
-                        '키 ${peoples[current].height}',
-                        textAlign: TextAlign.center,
+              AnimatedOpacity(
+                opacity: _opacity,
+                duration: Duration(seconds: 1),
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(width: 100, child: Text('이름 : ${peoples[current].name}'),),
+                      AnimatedContainer(
+                        duration: Duration(seconds: 2),
+                        curve: Curves.bounceIn,
+                        color: Colors.amber,
+                        child: Text(
+                          '키 ${peoples[current].height}',
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 50,
+                        height: peoples[current].height,
                       ),
-                      width: 50,
-                      height: peoples[current].height,
-                    ),
-                    AnimatedContainer(
-                      duration: Duration(seconds: 2),
-                      curve: Curves.easeInCubic,
-                      color: weightColor,
-                      child: Text(
-                        '몸무게 ${peoples[current].weight}',
-                        textAlign: TextAlign.center,
+                      AnimatedContainer(
+                        duration: Duration(seconds: 2),
+                        curve: Curves.easeInCubic,
+                        color: weightColor,
+                        child: Text(
+                          '몸무게 ${peoples[current].weight}',
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 50,
+                        height: peoples[current].weight,
                       ),
-                      width: 50,
-                      height: peoples[current].weight,
-                    ),
-                    AnimatedContainer(
-                      duration: Duration(seconds: 2),
-                      curve: Curves.linear,
-                      color: Colors.pinkAccent,
-                      child: Text(
-                        'bmi ${peoples[current].bmi}',
-                        textAlign: TextAlign.center,
+                      AnimatedContainer(
+                        duration: Duration(seconds: 2),
+                        curve: Curves.linear,
+                        color: Colors.pinkAccent,
+                        child: Text(
+                          'bmi ${peoples[current].bmi}',
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 50,
+                        height: peoples[current].bmi,
                       ),
-                      width: 50,
-                      height: peoples[current].bmi,
-                    ),
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                  ),
+                  height: 200,
                 ),
-                height: 200,
               ),
               ElevatedButton(
                 onPressed: () {
@@ -111,6 +116,14 @@ class _AnimationApp extends State<AnimationApp> {
                   });
                 },
                 child: Text('이전'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _opacity == 1 ? _opacity = 0 : _opacity = 1;
+                  });
+                },
+                child: Text('사라지기'),
               ),
             ],
             mainAxisAlignment: MainAxisAlignment.center,
